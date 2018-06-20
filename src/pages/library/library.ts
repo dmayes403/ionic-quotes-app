18bca8b567b6d76ca98a63ab6dc64a17e02573d2
dmayes403 <davidrmayes@gmail.com>
2018-06-19 22:32:03 -0600

    bringing in quotes

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,25 +1,14 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LibraryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import quotes from '../../data/quotes';
 @Component({
-  selector: 'page-library',
-  templateUrl: 'library.html',
+    selector: 'page-library',
+    templateUrl: 'library.html',
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit {
+    quoteCollection: {category: string, quotes: Quote[], icon: string}[]
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
-
+    ngOnInit() {
+        this.quoteCollection = quotes;
+    }
 }
